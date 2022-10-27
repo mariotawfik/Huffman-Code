82b8a03dec168a819445892165971b54559d7f2a
mariotawfik <amxmaroz@gmail.com>
2022-10-27 15:08:25 +0200

    Initial Commit

---
+++ Huffman Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Huffman Code
+//
+//  Created by Mary Tamry on 27.10.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
